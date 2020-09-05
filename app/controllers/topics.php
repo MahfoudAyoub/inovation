423d1030a598c6e9af7e868105a7d8358675a6ca
MahfoudAyoub <60109121+MahfoudAyoub@users.noreply.github.com>
2020-09-06 00:23:31 +0100

    commit

---
+++ app/controllers/topics.php
@@ -0,0 +1,37 @@
+<?php 
+include(ROOT_PATH . "/app/database/db.php");
+
+$table = 'topics';
+$id = '';
+$name = '';
+$description = '';
+
+$topics = selectAll($table);
+
+if(isset($_POST['add-topic'])){
+    unset($_POST['add-topic']);
+    $topic_id = create($table, $_POST);
+    $_SESSION['message'] = 'Topics created successfully';
+    $_SESSION['type'] = 'success';
+    header('location: ' . BASE_URL . '/admin/topics/index.php');
+    exit();
+}
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $topic = selectOne($table, ['id' => $id]);
+    $id = $topic['id'];
+    $name = $topic['name'];
+    $description = $topic['description'];
+
+}
+
+if(isset($_POST['update-topic'])){
+    $id = $_POST['id'];
+    unset($_POST['update-topic'], $_POST['id']);
+    $topic_id = update($table, $id, $_POST);
+    $_SESSION['message'] = 'Topics updatde successfully';
+    $_SESSION['type'] = 'success';
+    header('location: ' . BASE_URL . '/admin/topics/index.php');
+    exit();
+}
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,52 +1,59 @@
 $(document).ready(function () {
 
-    $('.menu-toggle').click(function () {
-      $('.menu-toggle').toggleClass('active');
-      $('nav').toggleClass('active');
-      $('nav ul').toggleClass('showing');
-    });
-
-    $('.posts-wrapper').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      nextArrow: $('.next'),
-      prevArrow: $('.prev'),
-      responsive: [{
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 880,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            infinite: true,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    });
+  $('.menu-toggle').click(function () {
+    $('.menu-toggle').toggleClass('active');
+    $('nav').toggleClass('active');
+    $('nav ul').toggleClass('showing');
   });
 
+  $('.posts-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 880,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
+  });
+});
 
-  ClassicEditor.create( document.querySelector( '#body' ) )
-              .then( editor => {
-                      console.log( editor );
-              } )
-              .catch( error => {
-                      console.error( error );
-              } );
+
+ClassicEditor
+  .create(document.querySelector('#description'), {
+    toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+      ]
+    }
+  })
+  .catch(error => {
+    console.log(error);
+  });
