114982122fab242f2c4942bb2cfc21ce8a84c0c3
MahfoudAyoub <60109121+MahfoudAyoub@users.noreply.github.com>
2020-09-14 17:26:18 +0100

    commit

--- chatRoom/admin/fetch_chat.php
+++ chatRoom/admin/fetch_chat.php
@@ -7,33 +7,45 @@ if (isset($_POST['fetch'])) {
 	$query = mysqli_query($conn, "select * from `chat` left join `users` on users.id=chat.userid where chatroomid='$id' order by chat_date asc") or die(mysqli_error());
 	while ($row = mysqli_fetch_array($query)) {
 ?>
-		<ul id="chat" >
-			<?php if ($_SESSION['id'] == $row['userid']) : ?>
-				<li class="me">
-					<div class="entete">
-						<h3><?php echo date('M-d-Y h:i A', strtotime($row['chat_date'])); ?></h3>
-						<h2><?php echo $row['uname']; ?></h2><img src="../<?php if(empty($row['photo'])){echo "upload/profile.jpg";}else{echo $row['photo'];} ?>">
+		<ul id="chat">
+			<?php if (isset($_SESSION['id'])) : ?>
+				<?php if ($_SESSION['id'] == $row['userid']) : ?>
+					<li class="me">
+						<div class="entete">
+							<h3><?php echo date('M-d-Y h:i A', strtotime($row['chat_date'])); ?></h3>
+							<h2><?php echo $row['uname']; ?></h2>
+							<img src="../
+							<?php if (empty($row['photo'])) {
+								echo "upload/profile.jpg";
+							} else {
+								echo $row['photo'];
+							} ?>">
 
-						<span class="status blue"></span>
-					</div>
-					<div class="triangle"></div>
-					<div class="message">
-						<?php echo $row['message']; ?>
-					</div>
-				</li>
-			<?php else : ?>
-				<li class="you">
-					<div class="entete">
-						<img src="../<?php if(empty($row['photo'])){echo "upload/profile.jpg";}else{echo $row['photo'];} ?>">
-						<h2><?php echo $row['uname']; ?></h2>
-						<span class="status blue"></span>
-						<h3><?php echo date('M-d-Y h:i A', strtotime($row['chat_date'])); ?></h3>
-					</div>
-					<div class="triangle"></div>
-					<div class="message">
-						<?php echo $row['message']; ?>
-					</div>
-				</li>
+							<span class="status blue"></span>
+						</div>
+						<div class="triangle"></div>
+						<div class="message">
+							<?php echo $row['message']; ?>
+						</div>
+					</li>
+				<?php else : ?>
+					<li class="you">
+						<div class="entete">
+							<img src="../<?php if (empty($row['photo'])) {
+												echo "upload/profile.jpg";
+											} else {
+												echo $row['photo'];
+											} ?>">
+							<h2><?php echo $row['uname']; ?></h2>
+							<span class="status blue"></span>
+							<h3><?php echo date('M-d-Y h:i A', strtotime($row['chat_date'])); ?></h3>
+						</div>
+						<div class="triangle"></div>
+						<div class="message">
+							<?php echo $row['message']; ?>
+						</div>
+					</li>
+				<?php endif; ?>
 			<?php endif; ?>
 		</ul>
 
